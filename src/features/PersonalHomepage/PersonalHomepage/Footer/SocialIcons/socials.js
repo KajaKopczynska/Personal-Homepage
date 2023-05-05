5c955f2fe22f03d31e4b3348c163a2832ba51094
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-05 12:45:33 +0200

    LinkedIn icon added

--- src/features/PersonalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/PersonalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,4 +1,5 @@
 import { ReactComponent as GithubIcon } from "./icons/github.svg";
+import { ReactComponent as LinkedinIcon} from "./icons/linkedIn.svg";
 import { styleIcon } from "./styled";
 
 export const socials = [
@@ -6,5 +7,11 @@ export const socials = [
         name: "GitHub",
         url: "https://github.com/KajaKopczynska",
         Icon: styleIcon(GithubIcon),
+    },
+
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/kaja-kopczy%C5%84ska-04153114a/",
+        Icon: styleIcon(LinkedinIcon),
     },
 ];
