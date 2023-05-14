98b78a1c9200a5cd57182187e5cbd895546edf34
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-05-14 17:41:51 +0200

    dispatch, selectors, and useEffect added in Portfolio

--- src/features/PersonalHomepage/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/PersonalHomepage/Portfolio/index.js
@@ -1,6 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Header, Section, StyledGithubIcon, SubHeader, SubTitle } from "./styled";
+import { selectRepositoriesStatus, selectRepositories, fetchRepositories } from "../personalHomepageSlice";
+import { useEffect } from "react";
 
 export const Portfolio = () => {
+    const dispatch = useDispatch();
+
+    const repositoriesStatus = useSelector(selectRepositoriesStatus);
+    const repositories = useSelector(selectRepositories);
+
+    useEffect(() => {
+        dispatch(fetchRepositories())
+    }, [dispatch]);
 
     return (
         <Section>
