8c26e8bb3f52d52263f7457949a3e66b23961281
KajaKopczynska <kaja.a.kopczynska@gmail.com>
2023-06-23 22:30:36 +0200

    hover photo added

--- src/features/PersonalHomepage/PersonalHomepage/Header/index.js
+++ src/features/PersonalHomepage/PersonalHomepage/Header/index.js
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import { Wrapper, Image, ThisIs, Name, Content, StyledButtonLink, HireMeIcon } from "./styled";
 import myPhoto from "./images/myPhoto.jpg";
+import myHoverPhoto from "./images/myHoverPhoto.jpg";
 import { email } from "../email";
 
-export const Header = () => (
-    <Wrapper>
-        <Image src={myPhoto} alt="Kaja Kopczyńska" />
-        <div>
-            <ThisIs>This is</ThisIs>
-            <Name>Kaja Kopczyńska</Name>
-            <Content>
-            Hi! I'm a novice Frontend Developer with a strong passion for exploring new technologies, particularly in the realm of React. I'm an ambitious individual with a keen ability to learn quickly. I'm currently searching for a chance to use my skills in a job. Although I don't have any work experience yet, I want to assure you that I am committed and eager to learn. I'm confident that I can quickly adapt and contribute effectively to any project or team.
-            </Content>
-            <StyledButtonLink href={`mailto:${email}`} title={email}>
-                <HireMeIcon />
-                Hire Me
-            </StyledButtonLink>
-        </div>
-    </Wrapper>
-);
+
+export const Header = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    const imageSrc = isHovered ? myHoverPhoto : myPhoto;
+
+    return (
+        <Wrapper>
+            <Image
+                src={imageSrc}
+                alt="Kaja Kopczyńska"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            />
+            <div>
+                <ThisIs>This is</ThisIs>
+                <Name>Kaja Kopczyńska</Name>
+                <Content>
+                    Hi! I'm a novice Frontend Developer with a strong passion for exploring new technologies, particularly in the realm of React. I'm an ambitious individual with a keen ability to learn quickly. I'm currently searching for a chance to use my skills in a job. Although I don't have any work experience yet, I want to assure you that I am committed and eager to learn. I'm confident that I can quickly adapt and contribute effectively to any project or team.
+                </Content>
+                <StyledButtonLink href={`mailto:${email}`} title={email}>
+                    <HireMeIcon />
+                    Hire Me
+                </StyledButtonLink>
+            </div>
+        </Wrapper>
+    );
+};
